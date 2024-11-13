456ccb5f118c067d0861146de4848b3dc998c027
Max Beringer <max.beringer@student.unisg.ch>
2024-11-13 21:04:18 +0100

    feat: add CRDT files

---
+++ FormulaEditor/src/main/java/org/example/formulaeditor/crdt/CRDTMerge.java
@@ -0,0 +1,16 @@
+package org.example.formulaeditor.crdt;
+
+import org.example.formulaeditor.model.Workbook;
+
+public class CRDTMerge {
+    private CRDTRules rules;
+
+    public CRDTMerge(CRDTRules rules) {
+        this.rules = rules;
+    }
+
+    //TODO merge logic
+    public Workbook merge(Workbook local, Workbook remote) {
+        return rules.applyRules(local, remote);
+    }
+}
---
+++ FormulaEditor/src/main/java/org/example/formulaeditor/crdt/CRDTRules.java
@@ -0,0 +1,11 @@
+package org.example.formulaeditor.crdt;
+
+import org.example.formulaeditor.model.Workbook;
+
+public class CRDTRules {
+
+    public Workbook applyRules(Workbook local, Workbook remote) {
+        //TODO implement merge rules
+        return local;
+    }
+}
