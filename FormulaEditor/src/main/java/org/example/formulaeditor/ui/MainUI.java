88bb12a2f8b3e5e39c14edfa5bfdc91b2da63dbe
Max Beringer <max.beringer@student.unisg.ch>
2024-11-20 18:26:07 +0100

    feat: add UI window

--- FormulaEditor/src/main/java/org/example/formulaeditor/ui/MainUI.java
+++ FormulaEditor/src/main/java/org/example/formulaeditor/ui/MainUI.java
@@ -1,4 +1,20 @@
 package org.example.formulaeditor.ui;
 
-public class MainUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        WorkbookUI workbookUI = new WorkbookUI();
+
+        Scene scene = new Scene(workbookUI, 800, 600);
+        primaryStage.setTitle("Formula Editor");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 }
+
--- FormulaEditor/src/main/java/org/example/formulaeditor/ui/WorkbookUI.java
+++ FormulaEditor/src/main/java/org/example/formulaeditor/ui/WorkbookUI.java
@@ -1,4 +1,21 @@
 package org.example.formulaeditor.ui;
 
-public class WorkbookUI {
+import javafx.beans.property.*;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+public class WorkbookUI extends BorderPane {
+    private final TableView<ObservableList<StringProperty>> tableView;
+
+    public WorkbookUI() {
+        this.tableView = new TableView<>();
+        initializeUI();
+    }
+
+    private void initializeUI() {
+
+        this.setCenter(tableView);
+    }
+
 }
