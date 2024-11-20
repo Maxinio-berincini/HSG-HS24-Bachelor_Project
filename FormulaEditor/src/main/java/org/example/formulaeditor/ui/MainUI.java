7939fe17669fac9cbb2414c937366004d8e669b6
Max Beringer <max.beringer@student.unisg.ch>
2024-11-20 22:51:04 +0100

    feat: add example with second window

--- FormulaEditor/src/main/java/org/example/formulaeditor/ui/MainUI.java
+++ FormulaEditor/src/main/java/org/example/formulaeditor/ui/MainUI.java
@@ -9,6 +9,7 @@ public class MainUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FormulaEditor formulaEditor = new FormulaEditor();
+        formulaEditor.addFormula("A1", "11 + 6");
         WorkbookUI workbookUI = new WorkbookUI(formulaEditor);
 
         Scene scene = new Scene(workbookUI, 800, 600);
@@ -16,6 +17,20 @@ public class MainUI extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        Stage secondStage = new Stage();
+        FormulaEditor formulaEditor2 = new FormulaEditor();
+        formulaEditor2.addFormula("A1", "10 + 15");
+        WorkbookUI workbookUI2 = new WorkbookUI(formulaEditor2);
+
+        Scene scene2 = new Scene(workbookUI2, 800, 600);
+        secondStage.setTitle("Formula Editor - Instance 2");
+        secondStage.setScene(scene2);
+        secondStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
 
