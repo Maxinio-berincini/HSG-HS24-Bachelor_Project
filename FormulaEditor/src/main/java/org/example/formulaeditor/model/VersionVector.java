94b216a7389a23898287ae7469b193e46d3971ae
Max Beringer <max.beringer@student.unisg.ch>
2024-11-27 14:46:47 +0100

    feat: add version vector

---
+++ FormulaEditor/src/main/java/org/example/formulaeditor/model/VersionVector.java
@@ -0,0 +1,72 @@
+package org.example.formulaeditor.model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class VersionVector{
+    private final Map<String, Integer> versions;
+
+    public VersionVector() {
+        versions = new HashMap<>();
+    }
+
+    public VersionVector(Map<String, Integer> versions) {
+        this.versions = new HashMap<>(versions);
+    }
+
+    public void increment(String InstanceId) {
+        versions.put(InstanceId, getVersion(InstanceId) + 1);
+    }
+
+    public int getVersion(String InstanceId) {
+        return versions.getOrDefault(InstanceId, 0);
+    }
+
+    public Map<String, Integer> getVersions() {
+        return versions;
+    }
+
+    public Set<String> getInstances() {
+        return versions.keySet();
+    }
+
+    public boolean isNewerVersion(VersionVector other) {
+        //TODO add logic to check if this is a newer version
+        /*
+        boolean atLeastOneGreater = false;
+        Set<String> allInstances = new HashSet<>();
+        allInstances.addAll(this.getInstances());
+        allInstances.addAll(other.getInstances());
+
+        for (String nodeId : allInstances) {
+            int thisVersion = getVersion(nodeId);
+            int otherVersion = other.getVersion(nodeId);
+
+            if (thisVersion < otherVersion) {
+                return false;
+            }
+            if (thisVersion > otherVersion) {
+                atLeastOneGreater = true;
+            }
+        }
+        return atLeastOneGreater;
+
+         */
+        return false;
+    }
+
+    public void merge(VersionVector other) {
+        for (String InstanceId : other.getInstances()) {
+            int otherVersion = other.getVersion(InstanceId);
+            int thisVersion = getVersion(InstanceId);
+            versions.put(InstanceId, Math.max(thisVersion, otherVersion));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return versions.toString();
+    }
+}
